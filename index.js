24f24443abf043fb0fd710f9c9100487c9b1914e
Shaline Davida <shalinedavida@gmail.com>
2025-06-05 08:41:41 +0300

    Created a new html file and added changes to it using dom

---
+++ index.js
@@ -0,0 +1,19 @@
+const body = document.body.style.backgroundColor ='silver';
+
+const title = document.getElementById('title').style.color = 'green';
+
+  document.querySelectorAll('h3').forEach(
+ element =>{
+    element.textContent = element.textContent.toUpperCase();
+ });
+   
+ const fruit = document.createElement('li');
+ fruList.appendChild(fruit);
+ fruit.textContent = ('Kiwi');
+
+const vegetable = document.createElement('li');
+ vegList.appendChild(vegetable);
+ vegetable.textContent = ('Brocolli');
+
+ 
+
